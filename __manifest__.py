5bf1e3347585ac9769e1aa19751d116ebebb5f64
Duy.N <vanduy1309@gmail.com>
2018-10-16 22:31:53 +0700

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name':'Theme 1',
+    'description':'A description for your theme',
+    'version':'1.0',
+    'author':'duynguyen',
+
+    'data': [
+        'views/layout.xml',
+        'views/pages.xml',
+        'views/assets.xml'   
+    ],
+    'category':'Theme/Creative',
+    'depends': ['website', 'website_sale', 'sale'],
+}
